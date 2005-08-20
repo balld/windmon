13b57a35abca917e75c5d68bfd28525576dddfdc
david <david>
2005-08-20 00:05:49 +0000

    First Version

---
+++ wind_monitor/windmon/ButtonExit.java
@@ -0,0 +1,36 @@
+/*
+ * Created on 20-Aug-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package windmon;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.Icon;
+
+
+/**
+ * @author David
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ButtonExit extends ActionButton {
+
+	JFrame fr;
+	
+	public ButtonExit(String caption, Icon img, JFrame frm)
+	{
+		super(caption, img);
+		this.fr = frm;
+	}
+	/* (non-Javadoc)
+	 * @see windmon.ActionButton#actionePerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e)
+	{
+		System.exit(0);
+	}
+}
