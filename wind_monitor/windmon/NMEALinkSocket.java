2e9148f5e55a590e6042e4f9a3adbdd7a913215c
david <david>
2005-10-01 19:36:58 +0000

    Renames from SocketNMEALink

---
+++ wind_monitor/windmon/NMEALinkSocket.java
@@ -0,0 +1,171 @@
+/*
+ * Created on Jul 31, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package windmon;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * @author david
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class NMEALinkSocket implements NMEALink, Runnable
+{
+    private int portNum = -1;
+    private String host = null;
+    private Socket nmeaSocket = null;
+    
+    private boolean openFlag = false;
+    
+    private InputStream is = null;
+    private BufferedReader br = null;
+    private PrintStream ps = null;
+    private Thread thread = null;
+    
+    private int linkReadTimeout = 10000; // 10 seconds
+
+    public NMEALinkSocket(String host, int portNum) {
+        this.host = host;
+    	this.portNum = portNum;
+    }
+
+    
+    public void start() {
+        if (thread == null) {
+        	thread = new Thread(this);
+            thread.setPriority(Thread.MIN_PRIORITY);
+            thread.start();
+        }
+    }
+
+    public synchronized void stop() {
+        if (thread != null) {
+            thread.interrupt();
+        }
+        thread = null;
+        notifyAll();
+    }
+
+    public void run() {
+        Thread me = Thread.currentThread();
+
+        while (thread == me && isOpen())
+        {
+        	Utils.justSleep(10000);
+        }
+        this.close();
+        thread = null;
+    }
+
+    
+    
+    public boolean open()
+    {
+    	if ( !isOpen() )
+    	{
+    		try
+			{
+    			this.nmeaSocket = new Socket(host, portNum);
+//    			nmeaSocket.setKeepAlive(false);
+    			nmeaSocket.setSoTimeout(linkReadTimeout);
+    			ps = new PrintStream(nmeaSocket.getOutputStream());
+    			is = nmeaSocket.getInputStream();
+    			br = new BufferedReader(new InputStreamReader(is));
+			}
+    		catch (Exception e)
+			{
+    			EventLog.log(EventLog.SEV_ERROR, "Could not open socket to " +
+    					host + " on port " + portNum + " : " +
+						e.getMessage());
+//    			e.printStackTrace();
+    			ps = null;
+    			is = null;
+    			br = null;
+			}
+    	}
+    	if ( isOpen() )
+    	{
+//    		this.start();
+			EventLog.log(EventLog.SEV_INFO, "Opened socket to " +
+					host + " on port " + portNum + " : ");
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    }
+    
+    public boolean close()
+    {
+        try {
+        	// Belt and braces - first close should close them all
+//        	br.close();
+//            is.close();
+//            ps.close();
+        	nmeaSocket.shutdownInput();
+        	nmeaSocket.shutdownOutput();
+            nmeaSocket.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Could not close socket");
+            e.printStackTrace();
+//            return false;
+        }
+        nmeaSocket = null;
+        ps = null;
+        is = null;
+        br = null;
+		EventLog.log(EventLog.SEV_INFO, "Closed socket to " +
+				host + " on port " + portNum + " : ");
+        return true;
+    }
+
+    public NMEAMessage getNMEAMessage()
+    {
+    	try
+		{
+            String cmd = br.readLine();
+            if ( cmd == null )
+            {
+            	throw new IOException("Null string read");
+            }
+//            EventLog.log(EventLog.SEV_DEBUG, "Read " + cmd);
+            return new NMEAMessage(cmd);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Exception reading NMEA message: " +
+                               e.getMessage());
+            // Primitive stuff, but if read fails we assume socket lost.
+            this.close();
+            return null;
+        }
+    }
+    
+    public void sendNMEAMessage(NMEAMessage msg)
+    {
+        try
+        {
+            ps.print(msg.getMessageString() + "\r\n");
+        }
+        catch (Exception ex)
+        {
+            System.err.println("Error send message to NMEA: "
+                                + msg.getMessageString());
+            ex.printStackTrace();
+        }
+    }
+    
+    public boolean isOpen()
+    {
+		return ( nmeaSocket != null && !nmeaSocket.isInputShutdown());
+    }
+}
