7a91df19b28d979f05599bc752af65dc1f427bc8
balld <david@ball1.net>
2013-10-15 01:16:09 +0100

    Mainly improvements to log formatting

--- wind_monitor/windmon/EventLog.java
+++ wind_monitor/windmon/EventLog.java
@@ -8,157 +8,167 @@ import java.text.FieldPosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class EventLog {
-	private static final Logger logger = Logger.getLogger(EventLog.class.getName());
-	public static final int SEV_DEBUG = 1;
-	public static final int SEV_INFO  = 2;
-	public static final int SEV_WARN  = 3;
-	public static final int SEV_ERROR = 4;
-	public static final int SEV_FATAL = 5;
-	public static final int SEV_MIN =   SEV_DEBUG;
-	public static final int SEV_MAX =   SEV_FATAL;
-	
-	public static final String sevStrings[] = { "XXXXX",
-		                                        "DEBUG",
-		                                        "INFO",
-		                                        "WARN",
-		                                        "ERROR",
-		                                        "FATAL" };
+  private static final Logger logger = Logger.getLogger(EventLog.class.getName());
+  public static final int SEV_DEBUG = 1;
+  public static final int SEV_INFO  = 2;
+  public static final int SEV_WARN  = 3;
+  public static final int SEV_ERROR = 4;
+  public static final int SEV_FATAL = 5;
+  public static final int SEV_MIN =   SEV_DEBUG;
+  public static final int SEV_MAX =   SEV_FATAL;
 
-	private static SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
-	private static SimpleDateFormat fmtFileName = new SimpleDateFormat("yyyyMMdd");
+  public static final String sevStrings[] = { "XXXXX",
+    "DEBUG",
+    "INFO",
+    "WARN",
+    "ERROR",
+  "FATAL" };
 
-	private static String AppLogDirectory = null;
-	
-	private static int logLevel = SEV_DEBUG;
-	
-	public static void log_old(int sev, String msg)
-	{
-		if ( sev >= logLevel)
-		{
-			StringBuffer bf = new StringBuffer();
-			fmt.format(new Date(System.currentTimeMillis()), bf, new FieldPosition(0));
-			
-			PrintStream ps = null;
-			if ( AppLogDirectory != null )
-			{
-				String fname =  AppLogDirectory + "/"
-					            + "windmon_" 
-				                + fmtFileName.format(new Date(System.currentTimeMillis())) 
-				                + ".log";
-				try {
-					ps = new PrintStream(new FileOutputStream(new File(fname), true));
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					ps = System.err;
-					AppLogDirectory = null;
-				}
-			}
-			else
-			{
-				ps = System.err;
-			}
+  private static SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+  private static SimpleDateFormat fmtFileName = new SimpleDateFormat("yyyyMMdd");
 
-			ps.println(bf + " " + sevToString(sev) + " " + msg );
-            
-            if (sev == SEV_FATAL )
-            {
-                Exception e = new Exception();
-                e.printStackTrace(ps);
-                System.exit(1);
-            }
-            
-            if ( ps != System.err )
-            {
-            	ps.flush();
-            	ps.close();
-            }
-		}
-	}
-	
-	private static String sevToString (int sev)
-	{
-		if ( sev < SEV_MIN || sev > SEV_MAX )
-		{
-			return sevStrings[0];
-		}
-		else
-		{
-			return sevStrings[sev];
-		}
-	}
-	
-	private static int stringToSev (String s)
-	{
-		int i = SEV_MAX;
-		
-		while ( i>= SEV_MIN && sevStrings[i].compareToIgnoreCase(s) != 0)
-		{
-			i--;
-		}
-		// Must have found match - or reached index of 0
-		return i;
-	}
-	
-	public static void setLogLevel(int level)
-	{
-		logLevel = level;
-	}
+  private static String AppLogDirectory = null;
 
-	public static void setLogLevelAsString(String s)
-	{
-		logLevel = stringToSev(s);
-	}
+  private static int logLevel = SEV_DEBUG;
 
-	public static void setAppLogDirectory(String s)
-	{
-		File path = new File(s);
-		if ( path.exists())
-		{
-			if (path.isDirectory())
-			{
-				AppLogDirectory = s;
-			}
-			else
-			{
-				AppLogDirectory = null;
-				logger.severe("AppLogDirectory '" + s + "' exists but is not a directory");
-				System.exit(1);
-			}
-		}
-		else
-		{
-			if ( path.mkdirs() == true )
-			{
-				logger.info("AppLogDirectory '" + s + "' created");
-				AppLogDirectory = s;
-			}
-			else {
-				AppLogDirectory = null;
-				logger.severe("AppLogDirectory '" + s + "' could not be created");
-				System.exit(1);
-			}				
-		}
-		try {
-			FileHandler handler = new FileHandler(AppLogDirectory + "/windmon.log", 1024000, 5, true);
-	        handler.setFormatter(new SimpleFormatter());
-	        Logger topLevelLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-	        topLevelLogger.addHandler(handler);
-	        topLevelLogger.setLevel(Level.INFO);
-	        // topLevelLogger.setUseParentHandlers(false);
-	        logger.info("Logging to '" + AppLogDirectory + "'");
-		} catch (Exception e) {
-			logger.warning("Failed to set up logging to log directory: " + e.getMessage());
-		}
-		
-	}
-	
-	public static int getLogLevel()
-	{
-		return logLevel;
-	}
+  public static void log_old(int sev, String msg)
+  {
+    if ( sev >= logLevel)
+    {
+      StringBuffer bf = new StringBuffer();
+      fmt.format(new Date(System.currentTimeMillis()), bf, new FieldPosition(0));
+
+      PrintStream ps = null;
+      if ( AppLogDirectory != null )
+      {
+        String fname =  AppLogDirectory + "/"
+            + "windmon_" 
+            + fmtFileName.format(new Date(System.currentTimeMillis())) 
+            + ".log";
+        try {
+          ps = new PrintStream(new FileOutputStream(new File(fname), true));
+        } catch (FileNotFoundException e) {
+          e.printStackTrace();
+          ps = System.err;
+          AppLogDirectory = null;
+        }
+      }
+      else
+      {
+        ps = System.err;
+      }
+
+      ps.println(bf + " " + sevToString(sev) + " " + msg );
+
+      if (sev == SEV_FATAL )
+      {
+        Exception e = new Exception();
+        e.printStackTrace(ps);
+        System.exit(1);
+      }
+
+      if ( ps != System.err )
+      {
+        ps.flush();
+        ps.close();
+      }
+    }
+  }
+
+  private static String sevToString (int sev)
+  {
+    if ( sev < SEV_MIN || sev > SEV_MAX )
+    {
+      return sevStrings[0];
+    }
+    else
+    {
+      return sevStrings[sev];
+    }
+  }
+
+  private static int stringToSev (String s)
+  {
+    int i = SEV_MAX;
+
+    while ( i>= SEV_MIN && sevStrings[i].compareToIgnoreCase(s) != 0)
+    {
+      i--;
+    }
+    // Must have found match - or reached index of 0
+    return i;
+  }
+
+  public static void setLogLevel(int level)
+  {
+    logLevel = level;
+  }
+
+  public static void setLogLevelAsString(String s)
+  {
+    logLevel = stringToSev(s);
+  }
+
+  public static void setAppLogDirectory(String s)
+  {
+    File path = new File(s);
+    if ( path.exists())
+    {
+      if (path.isDirectory())
+      {
+        AppLogDirectory = s;
+      }
+      else
+      {
+        AppLogDirectory = null;
+        logger.severe("AppLogDirectory '" + s + "' exists but is not a directory");
+        System.exit(1);
+      }
+    }
+    else
+    {
+      if ( path.mkdirs() == true )
+      {
+        logger.info("AppLogDirectory '" + s + "' created");
+        AppLogDirectory = s;
+      }
+      else {
+        AppLogDirectory = null;
+        logger.severe("AppLogDirectory '" + s + "' could not be created");
+        System.exit(1);
+      }				
+    }
+    try {
+      Logger topLevelLogger = Logger.getLogger("");
+      //
+      // Set formatter on default handler(s)
+      //
+      for (Handler h: topLevelLogger.getHandlers()) {
+        h.setFormatter(new LogFormatter());
+      }
+      
+      //
+      // Create file handler
+      //
+      FileHandler handler = new FileHandler(AppLogDirectory + "/windmon.log", 1024000, 5, true);
+      handler.setFormatter(new LogFormatter());
+      topLevelLogger.addHandler(handler);
+      topLevelLogger.setLevel(Level.INFO);
+      // topLevelLogger.setUseParentHandlers(false);
+      logger.info("Logging to '" + AppLogDirectory + "'");
+    } catch (Exception e) {
+      logger.warning("Failed to set up logging to log directory: " + e.getMessage());
+    }
+
+  }
+
+  public static int getLogLevel()
+  {
+    return logLevel;
+  }
 }
---
+++ wind_monitor/windmon/LogFormatter.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package windmon;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * @author David
+ *
+ */
+public class LogFormatter extends Formatter {
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd yyyy HH:mm:ss");
+  
+	/**
+	 * 
+	 */
+	public LogFormatter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+	 */
+	@Override
+	public String format(LogRecord rec) {
+	  return String.format("%s %-7s %9d %-30s %s\n", calcDate(rec.getMillis()), rec.getLevel().toString(), rec.getThreadID(), rec.getLoggerName(), formatMessage(rec));
+	}
+
+	private String calcDate(long millisecs) {
+		Date resultdate = new Date(millisecs);
+		synchronized(dateFormat) {
+		  return dateFormat.format(resultdate);
+		}
+	}
+
+
+}
